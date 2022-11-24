1f3a908f08d68a4b16bb7954f72ab5a69167f6c1
anm-jubaer <anm-jubaer@digigate360.com>
2022-11-24 17:08:44 +0600

    under dev

--- MagicVillaAPI/Program.cs
+++ MagicVillaAPI/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+
+using MagicVillaAPI.DBContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//builder.Services.AddDbContext<VillaDBContext>(opt => opt.UseInMemoryDatabase("TodoList"));
 
 var app = builder.Build();
 
