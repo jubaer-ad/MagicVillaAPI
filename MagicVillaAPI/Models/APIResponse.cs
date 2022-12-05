014532e99d5c670d5dae8695b58210be7f6e8d0f
anm-jubaer <anm-jubaer@digigate360.com>
2022-12-05 18:10:41 +0600

    APIResponse created

---
+++ MagicVillaAPI/Models/APIResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace MagicVillaAPI.Models
+{
+    public class APIResponse
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public bool IsSucces { get; set; }
+        public List<string> ErrorMessages { get; set; }
+        public object Result { get; set; }
+    }
+}
